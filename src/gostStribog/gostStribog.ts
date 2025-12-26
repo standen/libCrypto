fe0ab90915d1b867330d40ef078a66e4ded105b1
Standen <tkadensky@yandex.ru>
2025-12-26 20:30:40 +0300

    Init stribog

---
+++ src/gostStribog/gostStribog.ts
@@ -0,0 +1,17 @@
+import { Encoder } from "../encoding";
+
+export class Stribog {
+  private encode = new Encoder();
+
+  private X = () => {};
+
+  private S = () => {};
+
+  private P = () => {};
+
+  private L = () => {};
+
+  public hash = (text: string): string => {
+    return "";
+  };
+}
